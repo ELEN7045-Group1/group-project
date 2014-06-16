1e237cc66310d93ac1a6ab586dfc78315a81c349
paulvancoller <paul@vancoller.com>
2014-06-16 15:11:37 +0200

    Redefined Solution

--- SourceCode/AccountPresentationSystem/AccountPresentationSystem.Tests/Properties/AssemblyInfo.cs
+++ SourceCode/AccountPresentationSystem/AccountPresentationSystem.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AccountPresentationSystem")]
+[assembly: AssemblyTitle("AccountPresentationSystem.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AccountPresentationSystem")]
+[assembly: AssemblyProduct("AccountPresentationSystem.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0e3b9d81-11d3-4655-885a-05709a1ea3ad")]
+[assembly: Guid("73e6832c-733e-4edc-8de2-2f177b4a5ca3")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]

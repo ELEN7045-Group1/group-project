96972592bf40cf579eb57d4b54a5991f56ca5aad
paulvancoller <paul@vancoller.com>
2014-06-16 15:31:21 +0200

    Scheduler Init Commit

---
+++ SourceCode/AccountPresentationSystem/AccountPresentationSystem.Tests/Domain/Schedule/SchedulerServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AccountPresentationSystem.Domain.Schedule;
+
+namespace AccountPresentationSystem.Domain.Schedule
+{
+    [TestFixture]
+    public class SchedulerServiceTest
+    {
+        [Test]
+        public void CreateScheduleTest()
+        {
+            // Arrange
+            SchedulerService MyService = new SchedulerService();
+
+            // Act
+            bool retVal = MyService.CreateSchedule();
+
+            // Assert
+            Assert.IsTrue(retVal);
+        }
+    }
+}

dbc783799166d1993156522ddcee685a9e462abf
paulvancoller <paul@vancoller.com>
2014-06-18 11:18:59 +0200

    Package Updates

---
+++ SourceCode/WebApplication1/WebApplication1/Controllers/pvvcController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class pvvcController : Controller
+    {
+        // GET: pvvc
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public double Addition(double Val1, double Val2)
+        {
+            return Val1 + Val2;
+        }
+    }
+}

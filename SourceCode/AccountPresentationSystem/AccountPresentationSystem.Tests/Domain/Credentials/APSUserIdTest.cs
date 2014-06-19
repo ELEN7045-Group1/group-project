5b4c41252b40d85af96a1d37953ae9bd49664df2
iceman832 <patrickc3832@gmail.com>
2014-06-19 23:50:55 +0200

    Added more unit testing for APSUserId as well as added comments

--- SourceCode/AccountPresentationSystem/AccountPresentationSystem.Tests/Domain/Credentials/APSUserIdTest.cs
+++ SourceCode/AccountPresentationSystem/AccountPresentationSystem.Tests/Domain/Credentials/APSUserIdTest.cs
@@ -14,9 +14,36 @@
         [Test]
         public void  Create_APSUserId_ExpectionThrown()
         {
-            //APSUserId localAPSUserId = new APSUserId(null);
+            //Arrange -> Act -> Assert
+           Assert.Throws(typeof(ArgumentNullException), ()=> new APSUserId(null));
+        }
 
-            Assert.Throws(typeof(ArgumentNullException), ()=> new APSUserId(null));
+        [Test]
+        public void Create_APSUserId_ExpectValidId()
+        {
+            //Arrange
+            string localId = "abc123";
+            
+            //Act
+            APSUserId localAPSUserId = new APSUserId(localId);
+ 
+            //Assert
+            Assert.AreEqual(localAPSUserId.IdString, localId);
+        }
+
+        [Test]
+        public void Compare_APSUserId_ExpectObjectImmutability()
+        {
+            //Arrange
+            string localId1 = "abc123";
+            string localId2 = "abc123";
+
+            //Act
+            APSUserId localAPSUserId1 = new APSUserId(localId1);
+            APSUserId localAPSUserId2 = new APSUserId(localId2);
+
+            //Assert
+            Assert.AreEqual(localAPSUserId1.IdString, localAPSUserId2.IdString);
         }
     }
 }

55f1de34d839fd0664d8f9e0619087d11002cc9f
iceman832 <patrickc3832@gmail.com>
2014-06-20 23:12:05 +0200

    Added BillingAccountId and BillingCompanyId value objects and their unit tests

---
+++ SourceCode/AccountPresentationSystem/AccountPresentationSystem.Tests/Domain/Billing/BillingAccountIdTest.cs
@@ -0,0 +1,51 @@
+﻿namespace AccountPresentationSystem.Tests.Domain.Billing
+{
+    using AccountPresentationSystem.Domain.Billing;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    class BillingAccountIdTest
+    {
+        [Test]
+        public void Create_BillingAccountId_ExpectExceptionThrown()
+        {
+            //Arrange -> Act -> Assert
+            Assert.Throws(typeof(ArgumentNullException), () => new BillingAccountId(null));
+        }
+
+        [Test]
+        public void Create_BillingAccountId_ExpectValidId()
+        {
+            //Arrange
+            string localId = "abc123";
+
+            //Act
+            BillingAccountId localBillingAccountId = new BillingAccountId(localId);
+
+            //Assert
+            Assert.AreEqual(localBillingAccountId.IdString, localId);
+        }
+
+        [Test]
+        public void Compare_BillingAccountId_ExpectObjectImmutability()
+        {
+            //Arrange
+            string localId1 = "abc123";
+            string localId2 = "abc123";
+
+            //Act
+            BillingAccountId localBillingAccountId1 = new BillingAccountId(localId1);
+            BillingAccountId localBillingAccountId2 = new BillingAccountId(localId2);
+
+            //Assert
+            Assert.AreEqual(localBillingAccountId1.IdString, localBillingAccountId2.IdString);
+        }
+
+
+    }
+}
---
+++ SourceCode/AccountPresentationSystem/AccountPresentationSystem.Tests/Domain/Billing/BillingCompanyIdTest.cs
@@ -0,0 +1,49 @@
+﻿namespace AccountPresentationSystem.Tests.Domain.Billing
+{
+    using AccountPresentationSystem.Domain.Billing;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    class BillingCompanyIdTest
+    {
+        [Test]
+        public void Create_BillingCompanyId_ExpectionThrown()
+        {
+            //Arrange -> Act -> Assert
+            Assert.Throws(typeof(ArgumentNullException), () => new BillingCompanyId(null));
+        }
+
+        [Test]
+        public void Create_BillingCompanyId_ExpectValidId()
+        {
+            //Arrange
+            string localId = "abc123";
+
+            //Act
+            BillingCompanyId localBillingCompanyId = new BillingCompanyId(localId);
+
+            //Assert
+            Assert.AreEqual(localBillingCompanyId.IdString, localId);
+        }
+
+        [Test]
+         public void Compare_BillingCompanyId_ExpectObjectImmutability()
+        {
+            //Arrange
+            string localId1 = "abc123";
+            string localId2 = "abc123";
+
+            //Act
+            BillingCompanyId localBillingCompanyId1 = new BillingCompanyId(localId1);
+            BillingCompanyId localBillingCompanyId2 = new BillingCompanyId(localId2);
+
+             //Assert
+            Assert.IsTrue(localBillingCompanyId1.SameValueAs(localBillingCompanyId2));
+        }
+    }
+}

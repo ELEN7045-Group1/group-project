4cda59f61f317206132c4a817d0cbdcb4c4fbb98
iceman832 <patrickc3832@gmail.com>
2014-06-21 12:16:11 +0200

    Update Overall_Domain_Model_v7 and updated unit tests

--- SourceCode/AccountPresentationSystem/AccountPresentationSystem/Domain/Billing/BillingCompanyId.cs
+++ SourceCode/AccountPresentationSystem/AccountPresentationSystem/Domain/Billing/BillingCompanyId.cs
@@ -1,11 +1,11 @@
-﻿using NDDDSample.Domain.Shared;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace AccountPresentationSystem.Domain.Billing
+﻿namespace AccountPresentationSystem.Domain.Billing
 {
+    using NDDDSample.Domain.Shared;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
     public class BillingCompanyId : IValueObject<BillingCompanyId>
     {
         /// <summary>

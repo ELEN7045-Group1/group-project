fd7f1ba8cde655a4e517fb0957c003a7de2755a9
iceman832 <patrickc3832@gmail.com>
2014-06-21 14:02:15 +0200

    Folder Structure Re-arrangement

---
+++ SourceCode/AccountPresentationSystem/AccountPresentationSystem.Tests/Domain/Billing/BillingCompanyTest.cs
@@ -0,0 +1,37 @@
+﻿namespace AccountPresentationSystem.Tests.Domain.Billing
+{
+    using AccountPresentationSystem.Domain.Billing;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    class BillingCompanyTest
+    {
+        [Test]
+        public void Create_BillingCompany_ExceptionThrown()
+        {
+            //Arrange -> Act -> Assert
+            Assert.Throws(typeof(ArgumentNullException), () => new BillingCompany(null,null));
+        }
+
+        [Test]
+        public void Create_BillingCompany_ExpectValidCompany()
+        {
+            //Arrange
+            string billingCompanyIdString = "CoJ01";
+            string billingCompanyName = "City of Johannesburg";
+            BillingCompanyId billingCompanyId = new BillingCompanyId(billingCompanyIdString);
+
+            //Act
+            BillingCompany billingCompany = new BillingCompany(billingCompanyId, billingCompanyName);
+
+            //Assert
+            Assert.AreEqual(billingCompanyId, billingCompany);
+
+        }
+    }
+}
---
+++ SourceCode/AccountPresentationSystem/AccountPresentationSystem/Domain/Model/Billing/BillingCompany.cs
@@ -0,0 +1,53 @@
+﻿namespace AccountPresentationSystem.Domain.Billing
+{
+    using NDDDSample.Domain.Shared;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    
+    public class BillingCompany : IEntity<BillingCompany>
+    {
+        private readonly BillingCompanyId billingCompanyId;
+        private readonly string companyName;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BillingCompany"/> class.
+        /// </summary>
+        /// <param name="billingCompanyId">The billing company identifier.</param>
+        /// <param name="companyName">Name of the company.</param>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        public BillingCompany(BillingCompanyId billingCompanyId,string companyName)
+        {
+            if (billingCompanyId == null && string.IsNullOrEmpty(companyName))
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                this.billingCompanyId = billingCompanyId;
+                this.companyName = companyName;
+            }           
+        }
+
+        //public string IdString
+        //{
+        //    get { return id; }
+        //}
+
+        ////public string IdString
+        ////{
+        ////    get { return id; }
+        ////}
+
+        /// <summary>
+        /// Value objects compare by the values of their attributes, they don't have an identity.
+        /// </summary>
+        /// <param name="other">The other value object.</param>
+        /// <returns>true if the given value object's and this value object's attributes are the same.</returns>
+        public virtual bool SameIdentityAs(BillingCompany other)
+        {
+            return billingCompanyId.SameValueAs(other.billingCompanyId);
+        }
+    }
+}

3bc2179d137bbfb4e4331456bdfec0242a319d00
DerrickLew <diklew@gmail.com>
2014-06-21 09:18:48 +0200

    StatementHandler Changes + slight Domain Statement change

---
+++ SourceCode/AccountPresentationSystem/AccountPresentationSystem.Tests/Domain/StatementHandler/StatementTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AccountPresentationSystem.Domain.StatementHandler;
+
+namespace AccountPresentationSystem.Tests.Domain.StatementHandler
+{
+    [TestFixture]
+    class StatementTest
+    {
+        [Test]
+        public void CreateStatement_ExpectionThrown()
+        {
+            //Arrange -> Act -> Assert
+           Assert.Throws(typeof(ArgumentNullException), ()=> new Statement(null));
+        }
+        }
+    }
+}
---
+++ SourceCode/AccountPresentationSystem/AccountPresentationSystem/Domain/StatementHandler/Statement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccountPresentationSystem.Domain.StatementHandler
+{
+    public class Statement
+    {
+         /// <summary>
+        /// The identifier
+        /// </summary>
+        private readonly string scrapeddataobj;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Statement"/> class.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        public Statement(string scrapeddataobj)
+        {
+            if (!string.IsNullOrEmpty(scrapeddataobj))
+            {
+                this.scrapeddataobj = scrapeddataobj;   
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+    }
+}

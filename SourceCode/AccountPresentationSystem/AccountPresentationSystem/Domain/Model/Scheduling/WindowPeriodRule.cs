50064cc3b2beab9ca2dc9f4220eab5e7874d1c29
paulvancoller <paul@vancoller.com>
2014-06-22 11:45:24 +0200

    Added Schedule Rules

---
+++ SourceCode/AccountPresentationSystem/AccountPresentationSystem/Domain/Model/Scheduling/WindowPeriodRule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccountPresentationSystem.Domain.Model.Scheduling
+{
+    public class WindowPeriodRule : IScheduleRule
+    {
+        public bool RuleIsSatisfiedBy(Schedule schedule)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

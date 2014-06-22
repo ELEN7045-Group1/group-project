e771333070cd62989ccc80a02f93ebc386995a13
paulvancoller <paul@vancoller.com>
2014-06-22 23:37:56 +0200

    Added Missing file

---
+++ SourceCode/AccountPresentationSystem/AccountPresentationSystem/Domain/Service/SchedulerCreatorService.cs
@@ -0,0 +1,22 @@
+﻿using AccountPresentationSystem.Domain.Model.Scheduling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccountPresentationSystem.Domain.Service
+{
+    public class SchedulerCreatorService
+    {
+        public SchedulerCreatorService(ScheduleRepository scheduleRepo)
+        {
+
+        }
+
+        public bool CreateSchedule()
+        {
+            
+            return true;
+        }
+    }
+}

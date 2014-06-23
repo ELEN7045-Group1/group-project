7ad612cd7ce3ca9456d8bb3886c282e50e269a0e
kchristjustrang <kchristjustrang@gmail.com>
2014-06-23 22:38:15 +0200

    Added DI Custom Rule

--- SourceCode/AccountPresentationSystem/AccountPresentationSystem/Domain/Model/DataIntegrity/DataIntegrityRule.cs
+++ SourceCode/AccountPresentationSystem/AccountPresentationSystem/Domain/Model/DataIntegrity/DataIntegrityRule.cs
@@ -9,9 +9,40 @@
 
     public class DataIntegrityRule :IValueObject<DataIntegrityRule>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="APSUser"/> class.
+        /// </summary>
+        /// <param name="apsUserId">The aps user identifier.</param>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The password.</param>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        /// 
         public DataIntegrityRule()
         {
 
+        }
+
+        private bool checkResult;
+
+        public bool CheckResult
+        {
+            get { return checkResult; }
+            set { checkResult = value; }
+        }
+
+        public bool PerformAdditionCheck()
+        {
+            return CheckResult;
+        }
+
+        public bool PerformVATCheck()
+        {
+            return CheckResult;
+        }
+
+        public bool PerformDuplicateCheck()
+        {
+            return CheckResult;
         }
 
         public bool SameValueAs(DataIntegrityRule other)

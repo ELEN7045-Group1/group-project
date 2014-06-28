8774693da13297b5ba067ff28cedf969c75beec6
pdreyer <pietdreyer@gmail.com>
2014-06-28 13:55:16 +0200

    Added empty components for Full LM processes

---
+++ SourceCode/AccountPresentationSystem/AccountPresentationSystem/Application/Controllers/LoadManageerController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccountPresentationSystem.Application.Controllers
+{
+    public class LoadManageerController
+    {
+    }
+}
